c145406536b735cb3d51475352e671a5b757a5ad
Konosuke Sakai <vcbcitrus@gmail.com>
2026-01-29 21:46:51 +0900

    feat: Introduce Nix Flakes for environment management, add `wrangler` dev dependency, and enable the `ExplicitPublish` plugin.

--- quartz.config.ts
+++ quartz.config.ts
@@ -73,7 +73,7 @@ const config: QuartzConfig = {
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
-    filters: [Plugin.RemoveDrafts()],
+    filters: [Plugin.RemoveDrafts(), Plugin.ExplicitPublish()],
     emitters: [
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
