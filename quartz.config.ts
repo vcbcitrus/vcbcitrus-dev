be253e9a69094706bdd02ba2a9ac427f37c13e08
Konosuke Sakai <vcbcitrus@gmail.com>
2026-01-31 23:55:57 +0900

    config: Update baseUrl to vcbcitrus-dev.pages.dev.

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "vcbcitrus-dev.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
